ec28a125d3a568c2117a2ef8a7aed800ae1ffe4f
user <yulechkaakopyan@yandex.ru>
2023-03-09 16:32:59 +0300

    Курсовая

---
+++ src/main/java/com/example/kursovayafromsocks1/KursovayaFromSocks1Application.java
@@ -0,0 +1,13 @@
+package com.example.kursovayafromsocks1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursovayaFromSocks1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KursovayaFromSocks1Application.class, args);
+	}
+
+}
